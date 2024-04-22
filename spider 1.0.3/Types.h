2479ab129a7a5537f83090fda9f8cd4cd030d3c4
Simba-ExC <117112169+Simba-ExC@users.noreply.github.com>
2024-04-22 09:15:01 +0300

    Delete spider 1.0.3 directory

--- spider 1.0.3/Types.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-
-#include <string>
-#include <unordered_map>
-
-using WordMap = std::unordered_map<std::wstring, int>;
--- spider 1.0.3/UrlEncodeDecode/UrlEncodeDecode.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-#include <iostream>
-#include <sstream>
-#include <iomanip>
-
-std::string url_decode(const std::string& encoded);
-std::string url_encode(const std::string& decoted);
